2814829cf8216cb047c7003474138e7a05241fae
jordanbenbelaid <jordanbenbelaid@hotmail.com>
2021-02-05 10:07:39 +0000

    updated Billing solution

--- test4/src/test4/test4.java
+++ test4/src/test4/test4.java
@@ -42,48 +42,45 @@ public class test4 {
 			
 			int paid, bill, balance;
 
-			bill = 115;
-			paid = 320;
+			bill = 3;
+			paid = 84;
 			balance = paid - bill;
 			
 			int fifty, twenty, ten, five, two;
 			
 			if(balance>=50)
 			{
-				System.out.println(balance/50 + " 50s");
+				System.out.println((balance/50) + " 50s");
 				balance = balance%50;
-				
+			}
 				if(balance>=20)
 				{
-					System.out.println(balance/20 + " 20s");
+					System.out.println((balance/20) + " 20s");
 					balance = balance%20;
-					
+				}
 					if(balance>=10)
 					{
-						System.out.println(balance/10 + " 10s");
+						System.out.println((balance/10) + " 10s");
 						balance = balance%10;
-						
+					}		
 						if(balance>=5)
 						{
-							System.out.println(balance/5 + " 5s");
+							System.out.println((balance/5) + " 5s");
 							balance = balance%5;
-							
+						}		
 							if(balance>=2)
 							{
-								System.out.println(balance/2 + " 2s");
+								System.out.println((balance/2) + " 2s");
 								balance = balance%2;
-								
+							}		
 								if(balance>=1)
 								{
 									System.out.println(1 + " 1s");
-								}
+								}	
 								
-							}
-						}
-					}
-				}
-			}
+								
+								
 			
-			
+			}		
 	}
-}
+
---
+++ test5/src/test5/test5.java
@@ -0,0 +1,38 @@
+package test5;
+
+public class test5 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		
+		for(a=1; a<=10; a=a+1)
+		{
+			System.out.println(a + " Green Bottle");
+		}
+
+		System.out.println("==========================================");
+		
+		int ax, b, c;
+		
+		for(ax=1, b=10, c=100; ax <= 10; ax=ax+1, b=b+10, c=c+100)
+		{
+			System.out.println(ax + " " + b + " " + c);
+		}
+		
+		System.out.println("==========================================");
+		
+		int d, e, f;
+		
+		d=5;
+		e=10;
+		f=50;
+		
+		for( ; d<=50; d=d+5, e=e+10, f=f+50)
+		{
+			System.out.println(d + " " + e + " " + f);
+		}
+		
+		
+	}
+}
