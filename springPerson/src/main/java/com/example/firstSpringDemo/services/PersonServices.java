4ae452e782a7880e48a837ed4c28a55883efaeb8
jordanbenbelaid <jordanbenbelaid@hotmail.com>
2021-03-10 16:30:23 +0000

    person services done

--- springPerson/src/main/java/com/example/firstSpringDemo/services/PersonServices.java
+++ springPerson/src/main/java/com/example/firstSpringDemo/services/PersonServices.java
@@ -1,6 +1,9 @@
 package com.example.firstSpringDemo.services;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +25,51 @@ public class PersonServices {
 		List<Person> people = personRepository.findAll();
 		
 		return people;
+	}
+	
+	public Person readById(Integer id) {
+		Optional<Person> person = personRepository.findById(id);
+		if(person.isPresent()) {
+			return person.get();
+		} else {
+			throw new EntityNotFoundException();
+		}
+	}
+	
+	public Person createPerson(Person person) {
+		Person newPerson = personRepository.save(person);
+		
+		return newPerson;
+	}
+	
+	public Person updatePerson(Person person) {
+		Optional<Person> optionalPerson = personRepository.findById(person.getId());
+		Person updatedPerson;
+		
+		if(optionalPerson.isPresent()) {
+			updatedPerson = optionalPerson.get();
+		} else {
+			throw new EntityNotFoundException();
+		}
+		
+		updatedPerson.setAge(person.getAge());
+		updatedPerson.setHairColour(person.getHairColour());
+		updatedPerson.setHeight(person.getHeight());
+		updatedPerson.setName(person.getName());
+		updatedPerson.setSecrets(person.getSecrets());
+		
+		updatedPerson = personRepository.save(updatedPerson);
+		return updatedPerson;
+	}
+	
+	public boolean deletePerson(Integer id) {
+		if(!personRepository.existsById(id)) {
+			throw new EntityNotFoundException();
+		}
+		personRepository.deleteById(id);
+		
+		boolean exists = personRepository.existsById(id);
+		
+		return !exists;
 	}
 }
